8da58a508fcac8a8dcc6decca145307028424494
luckysher <luckyshera95@gmail.com>
2018-01-14 02:02:36 +0530

    updated blogger service

--- blogger_ui_services/blog.service.js
+++ blogger_ui_services/blog.service.js
@@ -9,6 +9,12 @@
           service.Blogs = getBlogs;
           return service;
 
+            // get the available blogs details
+            function getBlogs(callback){
 
+                  $http.get('http://127.0.0.1:8000/blogs', { headers : Headers }).then(function success(response){
+                                callback(response.data);
+                        },function error(response){ console.log("no blogs")});
+            }
         }
 })();
