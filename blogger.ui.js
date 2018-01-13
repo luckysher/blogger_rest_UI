276abc38d8733483a67b46f28de3a6602f2afcf7
luckysher <luckyshera95@gmail.com>
2018-01-13 18:58:21 +0530

    modified blogger ui js

--- blogger.ui.js
+++ blogger.ui.js
@@ -1,7 +1,9 @@
 (function(){
     'use strict';
-
-
+        angular
+                .module('bloggerui', ['ui.router', 'ngMessages', 'ngStorage'])
+                .config(config)
+                .run(run);
          
           
 })();
