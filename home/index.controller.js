13d09ec74374907af77cf61f4420235be61d590b
luckysher <luckyshera95@gmail.com>
2018-02-03 17:53:40 +0530

    updated controller stmt

--- home/index.controller.js
+++ home/index.controller.js
@@ -2,7 +2,11 @@
     'use strict';
     angular
         .module('bloggerui')
-        
+        .controller('Home.IndexController', homeController);
+
+        function homeController(){
+
+        };
             
           
 })();
