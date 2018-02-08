abcb835ed5bff7978046828601ec55a5ee04dec2
luckysher <luckyshera95@gmail.com>
2018-02-08 11:57:58 +0530

    updated blogger ui js

--- blogger.ui.js
+++ blogger.ui.js
@@ -1,30 +1,29 @@
 (function(){
     'use strict';
         angular
-                .module('bloggerui', ['ui.router', 'ngMessages', 'ngStorage'])
-                .config(config)
-                .run(run);
+            .module('bloggerui', ['ngRoute', 'ngMessages', 'ngStorage'])
+            .config(config)
+            .run(run);
 
         function config($routeProvider) {
+                    console.log("loaded config...");
 
-                    $routeProvider.
-                            when('/blogs', {
+                    $routeProvider
+                            .when('/blogs', {
                                             templateUrl: 'home/home.view.html',
                                             controller: 'Home.IndexController',
                                             controllerAs: 'blogger'
-                                        }).
-                            when('/blog/:Id', {
-                                            template: 'blog/blog.details.html',
+                                        })
+                            .when('/blog/Id', {
+                                            templateUrl: 'blog/blog.details.html',
                                             controller: 'Blog.IndexController',
                                             controllerAs: 'blogger'
-                                        }).
-                            otherwise('/blogs');  // default url
-                                    }
+                                        })
+                            .otherwise('/blog/Id');  // default url
+                          }
 
 
         function run(){
             console.log("Running...");
-        };
-
-          
+        }
 })();
